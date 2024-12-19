f888a5858b934112c98b75ae4aa51bdfd8746609
Erik Garces Matos <erik_senac@hotmail.com>
2024-12-19 13:38:35 -0300

    config: Changing python formatter from autopep8 to black

--- redis_raw.py
+++ redis_raw.py
@@ -2,34 +2,34 @@ from typing import Optional
 
 import redis
 
-redis_conn = redis.Redis(host='localhost', port=6379, db=0)
+redis_conn = redis.Redis(host="localhost", port=6379, db=0)
 
 # para inert e update
-redis_conn.set('chave_1', 'trocando o meu valor')
+redis_conn.set("chave_1", "trocando o meu valor")
 
 # select
 valor: Optional[bytes] = redis_conn.get("chave_1")
 if valor is not None:
-    valor = valor.decode('utf-8')
+    valor = valor.decode("utf-8")
 
 # delete dados
-redis_conn.delete('chave_1')
+redis_conn.delete("chave_1")
 
 
 # comandos para hash
-redis_conn.hset('meu_hash', 'nome', 'joao')
-redis_conn.hset('meu_hash', 'idade', '30')
-redis_conn.hset('meu_hash', 'cidade', 'curitiba')
+redis_conn.hset("meu_hash", "nome", "joao")
+redis_conn.hset("meu_hash", "idade", "30")
+redis_conn.hset("meu_hash", "cidade", "curitiba")
 
-valor_1 = redis_conn.hget('meu_hash', 'nome').decode("utf-8")
+valor_1 = redis_conn.hget("meu_hash", "nome").decode("utf-8")
 
-redis_conn.hdel('meu_hash', 'cidade')
+redis_conn.hdel("meu_hash", "cidade")
 
 # buscas por existencia chaves normais
-elem = redis_conn.exists('chave_1')
+elem = redis_conn.exists("chave_1")
 print(elem)
 # buscas por existencia chaves hashes
-elem2 = redis_conn.hexists('meu_hash', "nome")
+elem2 = redis_conn.hexists("meu_hash", "nome")
 print(elem2)
 
 # Expiracão de dados TTL -> Time To Live
